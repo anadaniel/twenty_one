50be3066d6c151dc42304182f2e7300440e4d7d7
Ana Daniel <ana.daniel@live.com>
2015-07-24 14:19:03 -0500

    Add api constraints class

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
 
   # Change the line below to handle every request through a subdomain
   #namespace :api, constraints: { subdomain: 'api' }, path: '/' do
-  namespace :api, path: '/api' do
-    api_version(:module => "V1", :header => {:name => "Accept",
-                                             :value => "application/vnd.twenty_one.com+json; version=1"},
-                                             :defaults => {:format => :json}, :default => true) do
+
+  namespace :api, defaults: { format: :json}, path: '/api' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
     end
   end
+  # namespace :api, path: '/api' do
+  #   api_version(:module => "V1", :header => {:name => "Accept",
+  #                                            :value => "application/vnd.twenty_one.com+json; version=1"},
+  #                                            :defaults => {:format => :json}, :default => true) do
+  #   end
+  # end
 
 end
