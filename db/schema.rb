d886757b65f90f760c36df13765db0be157328a6
Ana Daniel <ana.daniel@live.com>
2015-07-24 17:00:14 -0500

    Adds user auth token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724194416) do
+ActiveRecord::Schema.define(version: 20150724195931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,10 @@ ActiveRecord::Schema.define(version: 20150724194416) do
     t.datetime "reset_password_sent_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "auth_token",             default: ""
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,28 @@ RSpec.describe User, :type => :model do
     it { is_expected.to respond_to(:email) }
     it { is_expected.to respond_to(:password) }
     it { is_expected.to respond_to(:password_confirmation) }
+    it { is_expected.to respond_to(:auth_token) }
+  end
+
+  context "validations" do
+    it { is_expected.to validate_uniqueness_of(:auth_token) }
+  end
+
+  context "#methods" do
+    describe "#generate_authentication_token!" do
+      before { @user = FactoryGirl.create :user }
+      
+      it "generates a unique token" do
+        Devise.stub(:friendly_token).and_return("auniquetoken123")
+        @user.generate_authentication_token!
+        expect(@user.auth_token).to eql "auniquetoken123"
+      end
+
+      it "generates another token when one already has been taken" do
+        existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+        @user.generate_authentication_token!
+        expect(@user.auth_token).not_to eql existing_user.auth_token
+      end
+    end
   end
 end
